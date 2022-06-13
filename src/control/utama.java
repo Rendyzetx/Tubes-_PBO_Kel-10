f55e0e372448f9bc2c94acd528b96828ad4d470b
Rendyzetx <107420488+Rendyzetx@users.noreply.github.com>
2022-06-14 02:11:28 +0800

    Update utama.java

--- src/control/utama.java
+++ src/control/utama.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package control;
 
 import gui.FormUtama;
@@ -10,10 +5,6 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author Acer
- */
 public class utama extends koneksi{
     public utama(){
         super.setKoneksi();
