62d5da688f3aa7c8cf78d32435d22aa941b2f3bd
Rendyzetx <107420488+Rendyzetx@users.noreply.github.com>
2022-06-17 20:50:33 +0800

    Update frmCariBarang.java

--- src/gui/frmCariBarang.java
+++ src/gui/frmCariBarang.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gui;
 
 
@@ -11,10 +7,7 @@ import control.masterBarang;
 import control.pelanggan;
 
 
-/**
- *
- * @author Acer
- */
+
 public class frmCariBarang extends javax.swing.JDialog {
     masterBarang mb;
     pelanggan pl;
